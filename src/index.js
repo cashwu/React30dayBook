f3097ce3de3ef8047e5ceb2c1f7438bb18f00581
cash <cashwugeek@icloud.com>
2024-07-10 21:46:25 +0800

    ch2-3

--- src/index.js
+++ src/index.js
@@ -5,13 +5,29 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+setInterval(() => {
+
+  const reactElement = React.createElement(
+    'div',
+    null,
+    React.createElement('h1', null, 'hello world', 'aaa', 'bbb'),
+    React.createElement('h2', null, 'time is ', new Date().toLocaleTimeString()),
+  );
+
+  root.render(reactElement)
+}, 1000);
+
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// reportWebVitals();
