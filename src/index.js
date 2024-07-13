22579dbf194d925124d6c70d9e9b4c854e8c6324
cash <cashwugeek@icloud.com>
2024-07-13 21:57:38 +0800

    ch2-7 component

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 // import React from 'react';
 // import {Fragment} from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import MyComponent from "./MyComponent";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // const items =["foo", "bar"];
-const items =["bar"];
+const items = ["bar"];
 
 let childElement;
-if (items.length >1) {
-  childElement = <input type='number' name='number' />
+if (items.length > 1) {
+  childElement = <input type="number" name="number" />;
 } else {
-  childElement = <input type='password' name='password' />
+  childElement = <input type="password" name="password" />;
 }
 
-  const reactElement = (
-    <>
-      <div id="wrapper" className='foo'>
-        <ul>
-          {items.map(item => <li className='list-item'>{item}</li>)}
-          {/* <li className='list-item'>item 1</li>
+function MyButton() {
+  return <button>i am a button</button>;
+}
+
+const reactElement = (
+  <>
+    <MyComponent title="hello world" subTitle="sub title" />
+    <div id="wrapper" className="foo">
+      <ul>
+        {items.map((item) => (
+          <li className="list-item">{item}</li>
+        ))}
+        {/* <li className='list-item'>item 1</li>
           <li className='list-item'>item 2</li>
           <li className='list-item'>item 3</li> */}
-        </ul>
-        
-        {childElement}
+      </ul>
 
-        <button id='button1'>i am a button</button>
-      </div>
-      <div>1213</div>
-    </>
-  );
+      {childElement}
 
-  root.render(reactElement)
+      <MyButton />
+      {/* <button id='button1'>i am a button</button> */}
+    </div>
+    <div>1213</div>
+  </>
+);
 
-
+root.render(reactElement);
 
 // root.render(
 //   <React.StrictMode>
