b044c5a3860c7cda7888f7123213268db97cae42
Cash <cashwugeek@icloud.com>
2024-07-17 15:51:56 +0800

    update style

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import reactLogo from "../images/logo192.png"
 export default function Footer() {
 
     return (
-        <footer className={ styles }>
+        <footer className={ styles['app-footer'] }>
             Cash @ Copyright 2024
             <img src={reactLogo} className="footer-logo" alt="logo" />
         </footer>
